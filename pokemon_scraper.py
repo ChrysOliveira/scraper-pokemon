0d04f104fd5db6b6b4c5c6f277904ba00b67b5f8
Chrystian Oliveira <chrystianmoliveira7@gmail.com>
2024-08-27 17:35:04 -0300

    habilidades ok
    precisamos urgentemente organizar esse cod mds do ceu

--- pokemon_scraper.py
+++ pokemon_scraper.py
@@ -9,10 +9,10 @@ class PokemonScrapper(scrapy.Spider):
 
     def parse(self, response):
         pokemons = response.css('#pokedex > tbody > tr')
-        # for pokemon in pokemons:
-        pokemon = pokemons[0]
-        link = pokemon.css("td.cell-name > a::attr(href)").extract_first()
-        yield response.follow(self.domain + link, self.parse_pokemon, dont_filter=True)
+        for pokemon in pokemons:
+            # pokemon = pokemons[0]
+            link = pokemon.css("td.cell-name > a::attr(href)").extract_first()
+            yield response.follow(self.domain + link, self.parse_pokemon, dont_filter=True)
 
     def parse_pokemon(self, response):
         prefix_domain = "https://pokemondb.net"
@@ -38,20 +38,49 @@ class PokemonScrapper(scrapy.Spider):
         for habilidade in lista_habilidades:
             links_habilidades.append(prefix_domain + habilidade.css("a::attr(href)").get())
 
-        print(f"AAAAAAA: {links_habilidades}")
         habilidades = Request(links_habilidades[0], callback=self.ability_data, dont_filter=True)
         habilidades.meta['links_habilidades_pendentes'] = links_habilidades[1:]
         habilidades.meta['lista'] = []
+        habilidades.meta['links_evolucoes'] = links_evolucoes
+        habilidades.meta['dados_principal'] = response
+        habilidades.meta['poke_tipos'] = poke_tipos
+        habilidades.meta['table_path'] = table_path
         yield habilidades
-        print(f"BBBBBBB: {links_habilidades}")
 
-        if links_evolucoes:
-            request_evo = Request(links_evolucoes[0], callback=self.evolution_data, dont_filter=True)
-            request_evo.meta['pokemon_dados'] = self.getting_data(response, poke_tipos, table_path, habilidades.meta['lista'])
-            request_evo.meta['links_evolucoes_pendentes'] = links_evolucoes[1:]
-            yield request_evo
+    def ability_data(self, response):
+        links_habilidades_pendentes = response.meta['links_habilidades_pendentes']
+        links_evolucoes = response.meta['links_evolucoes']
+
+        ability_info = {
+            'pokemon_url': response.css('link[rel="canonical"]::attr(href)').get(),
+        }
+
+        response.meta['lista'].append(ability_info)
+
+        if links_habilidades_pendentes:
+            next_request = Request(links_habilidades_pendentes[0], callback=self.ability_data, dont_filter=True)
+            next_request.meta['links_habilidades_pendentes'] = links_habilidades_pendentes[1:]
+            next_request.meta['lista'] = response.meta['lista']
+            next_request.meta['links_evolucoes'] = response.meta['links_evolucoes']
+            next_request.meta['dados_principal'] = response.meta['dados_principal']
+            next_request.meta['poke_tipos'] = response.meta['poke_tipos']
+            next_request.meta['table_path'] = response.meta['table_path']
+            yield next_request
+
         else:
-            yield self.getting_data(response, poke_tipos, table_path, habilidades.meta['lista'])
+            if links_evolucoes:
+                request_evo = Request(links_evolucoes[0], callback=self.evolution_data, dont_filter=True)
+                request_evo.meta['pokemon_dados'] = self.getting_data(response.meta['dados_principal'],
+                                                                      response.meta['poke_tipos'],
+                                                                      response.meta['table_path'],
+                                                                      response.meta['lista'])
+                request_evo.meta['links_evolucoes_pendentes'] = links_evolucoes[1:]
+                yield request_evo
+            else:
+                yield self.getting_data(response.meta['dados_principal'],
+                                        response.meta['poke_tipos'],
+                                        response.meta['table_path'],
+                                        response.meta['lista'])
 
     def evolution_data(self, response):
         pokemon_dados = response.meta['pokemon_dados']
@@ -73,23 +102,6 @@ class PokemonScrapper(scrapy.Spider):
         else:
             yield pokemon_dados
 
-    def ability_data(self, response):
-        links_habilidades_pendentes = response.meta['links_habilidades_pendentes']
-
-        ability_info = {
-            'pokemon_url': response.css('link[rel="canonical"]::attr(href)').get(),
-        }
-
-        response.meta['lista'].append(ability_info)
-
-        if links_habilidades_pendentes:
-            next_request = Request(links_habilidades_pendentes[0], callback=self.ability_data, dont_filter=True)
-            next_request.meta['links_habilidades_pendentes'] = links_habilidades_pendentes[1:]
-            next_request.meta['lista'] = response.meta['lista']
-            yield next_request
-        else:
-            yield response.meta['lista']
-
     def getting_data(self, response, poke_tipos, table_path, habilidades):
         return {
             'pokemon_id': response.css(f'{table_path} > tr:nth-child(1) > td > strong::text').get(),
@@ -97,7 +109,8 @@ class PokemonScrapper(scrapy.Spider):
             'pokemon_name': response.css('#main > h1::text').get(),
             'next_evolutions': [],  # TODO: remover des-evolucoes
             'pokemon_size': str(
-                (float(response.css(f'{table_path} > tr:nth-child(4) > td::text').get().split(" ", 1)[0]) * 100)) + ' cm',
+                (float(
+                    response.css(f'{table_path} > tr:nth-child(4) > td::text').get().split(" ", 1)[0]) * 100)) + ' cm',
             # TODO: arredondar o valor para 2 casas
             'pokemon_weight': response.css(f'{table_path} > tr:nth-child(5) > td::text').get().split(" ", 1)[0] + ' kg',
             'pokemon_types': poke_tipos,
